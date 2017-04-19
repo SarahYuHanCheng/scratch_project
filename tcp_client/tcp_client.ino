cbdc459dfaa02eadb4f4981825c8a852ff5427e8
SarahYuHanCheng <bigbigtreeful@gmail.com>
2017-04-19 23:46:25 +0800

    change to use WeMos

--- tcp_client/tcp_client.ino
+++ tcp_client/tcp_client.ino
@@ -1,23 +1,25 @@
+#include <ESP8266WiFi.h>
+
+#include <WiFiClient.h>
+
 /* A TCP client.
  */
-#include <KSM111_ESP8266.h>
-#include <SoftwareSerial.h>
-
- KSM111_ESP8266 wifi(3, 2);  // USe SoftwareSerial: Rx pin 3, Tx pin 2.
-
+WiFiClient wifiClient;
 
  // You have to modify the following parameters.
-#define AP_SSID         "scream"
-#define AP_PASSWD       "s741852scream"
-#define TCP_SERVER_IP   "192.168.1.205"
-#define TCP_SERVER_PORT 8888
+#define SSID "scream"
+#define PASSWD "s741852scream"
+#define TCP_IP "192.168.1.205"
+#define TCP_PORT 8888
 
 #define deviceID 12
+int ledpin =D5;
 char _buffer[3];
 
 void setup()
 {
-  
+   pinMode(ledpin, OUTPUT);
+   pinMode(2, OUTPUT);
   int idflag =1;
   char joinedSSID[32];
 
@@ -25,42 +27,21 @@ void setup()
   Serial.begin(9600);
   while (!Serial)
     ;
-    pinMode(13,OUTPUT);
-  wifi.begin(9600);
-
-  Serial.print("Connecting to the AP... ");
-  // Check if the module joined an AP.
-  if (!wifi.joinedAP(joinedSSID) ||
-      strcmp(joinedSSID, AP_SSID) != 0) {
-    // Quit the current connected AP and connect to the new one.
-    wifi.quitAP();
-    wifi.setMode(STATION);
-    wifi.multiConnect(false);
-    if (wifi.joinAP(AP_SSID, AP_PASSWD) < 0) {
-      // Cannot connect to the AP, stop.
-      Serial.println("Fail");
-      while (1)
-        ;
+   
+   Serial.println("Connecting...");
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(SSID, PASSWD);
+    while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+        WiFi.begin(SSID, PASSWD);
+        Serial.println("Retrying...connect to AP");
     }
-    
-  }
-
-  Serial.print("OK\nJoining to the TCP server... ");
-  
-  if (wifi.beginClient("TCP", TCP_SERVER_IP, TCP_SERVER_PORT) != CONNECT_ERROR)
-    {
-      delay(500);
-      _buffer[0]='S';
+    Serial.println("Connected to AP");
+     wifiClient.connect(TCP_IP, TCP_PORT);
+     _buffer[0]='S';
       String(deviceID, HEX).toCharArray((_buffer+1), 2);
-      wifi.puts(_buffer);
-      Serial.println("sended id OK");
-    }
-  else {
-    // Cannot join to the TCP server, stop.
-    Serial.println("Fail");
-    while (1)
-      ;
-  }
+      wifiClient.write(_buffer,2);
+     Serial.println("sended id");
+  
 }
 
 static char buffer[32];
@@ -70,34 +51,37 @@ void loop()
   int charAvail;
   
   // gets() would return the ID of the sender.
-  if ((charAvail=wifi.gets(buffer, 8)) >= 0) {
-      if(buffer[0]=='1'){ digitalWrite(13,HIGH);
+  if ((charAvail = wifiClient.available()) >0) {
+    wifiClient.read((unsigned char *)buffer, 32);
+      if(buffer[0]=='1'){ digitalWrite(2,HIGH);//BUILTIN_LED off
       Serial.println(buffer[0]);}
-      else if(buffer[0]=='0'){digitalWrite(13,LOW);}
+      else if(buffer[0]=='0'){digitalWrite(2,LOW);}//BUILTIN_LED on
       
       for(int i=1;i<sizeof(buffer);i++){ buffer[i]={0}; }
   }
   
-
   if (millis()-timerCheckUpdate>=10000)
   {
-        if(!wifi.isClientConnected()){
-          if (wifi.beginClient("TCP", TCP_SERVER_IP, TCP_SERVER_PORT) != CONNECT_ERROR)
-          {
-            _buffer[0]='S';
-             String(deviceID, HEX).toCharArray((_buffer+1), 2);
-            wifi.puts(_buffer);
-          }
-        else {
-          // Cannot join to the TCP server, stop.
-          Serial.println("Fail");
-          while (1)
-            ;
-        }
-      }else { 
-        //Serial.println("is connected");
+        if(!wifiClient.connected()){
+           // Cannot join to the TCP server, stop.
+           if(!wifiClient.connect(TCP_IP, TCP_PORT)){
+              Serial.println("connection failed");
+               int i=0;
+               while(i<2){
+                 digitalWrite(ledpin,HIGH);
+                 delay(800);
+                 digitalWrite(ledpin,LOW);
+                 delay(200);
+                 i++;
+               }
+              //return; 
+            }else{
+             _buffer[0]='S';
+              String(deviceID, HEX).toCharArray((_buffer+1), 2);
+              wifiClient.write(_buffer,2);
+             Serial.println("sended id again");
+            }
         }
     timerCheckUpdate=millis();
   }
 }
-
