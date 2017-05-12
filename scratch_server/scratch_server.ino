8ec548db065cfbd61e4d755ce2242ce255cba7c9
SarahCheng <bigbigtreeful@gmail.com>
2017-05-12 16:48:07 +0800

    no readSerialport

--- scratch_server/scratch_server.ino
+++ scratch_server/scratch_server.ino
@@ -1,13 +1,22 @@
 //test ack pin10 with sensor on
 #include <SoftwareSerial.h>
-SoftwareSerial showmsg(3, 2);
+SoftwareSerial wifitoserver(3, 2);
 byte handshaking_pin = 0;//A0
 byte msg_ID_pin = 2; //A2 msg
 byte msg_act_pin = 1; //A1
+byte act_back = 3; //A3
+
 
 int the_value,msg_ID;
-
 byte scratch_ack = 10;
+
+bool flag = true;
+static int char_count = 0;
+char cat[10];
+String deviceID;
+int _ID[0];
+char device_ctr;
+
 typedef enum {  digital//  input, servomotor, pwm,
  }pinType;
 
@@ -23,8 +32,8 @@ void setup()
 {
   Serial.begin(38400);
   Serial.flush();
-  showmsg.begin(9600);
-  showmsg.flush();
+  wifitoserver.begin(9600);
+  wifitoserver.flush();
   delay(10);
   configurePins();
   msg_ID=12;
@@ -32,16 +41,25 @@ void setup()
 
 void loop()
 {
-  readSerialPort();
-  delay(10);
   recvwifi();
-  delay(200);
-  
+  delay(10);
 }
 void recvwifi() {
-  ScratchBoardSensorReport(msg_act_pin,1);
-  ScratchBoardSensorReport(msg_ID_pin,msg_ID);
+   if ((char_count = wifitoserver.available()) > 0) { //recv msg from server
+    for (int i = 0; i < char_count; ++i) {
+    cat[i] = wifitoserver.read(); //char mssg[]
+     if (i == 0) {
+        msg_ID = strtoul(cat, NULL, 16); //ID
+      }
+    }
+
+    ScratchBoardSensorReport(msg_ID_pin,msg_ID);
+    for (int i = 1; i < sizeof(cat); i++) { cat[i] = {0};}
+    delay(20);
+  }else{ScratchBoardSensorReport(handshaking_pin,0);
+    }
   }
+  
 void configurePins()
 {
   for (int index = 7; index < 10; index++)
@@ -50,7 +68,6 @@ void configurePins()
     arduinoPins[index].state = 0;
     pinMode(index, OUTPUT);
     digitalWrite(index, LOW); //reset pins
-    
   }
    pinMode(10, INPUT_PULLUP);
 }
@@ -63,54 +80,19 @@ void ScratchBoardSensorReport(byte sensor, int value) //PicoBoard protocol, 2 by
   Serial.write( value & B1111111);
 }
 
-void readSerialPort()
+void checkScratchDisconnection()
 {
-  ScratchBoardSensorReport(handshaking_pin, 0);
-  int pin, inByte, sensorHighByte, states;
-
-  if (Serial.available() > 1)
-  {
-    lastDataReceivedTime = millis();
-    inByte = Serial.read();
-    if (inByte >= 128) // Are we receiving the word's header?
-    {
-      sensorHighByte = inByte;
-      pin = ((inByte >> 3) & 0x0F);
-      showmsg.println(pin);
-      if (pin == scratch_ack) {
-        while (!Serial.available()); // Wait for the end of the word with data
-        inByte = Serial.read();
-        
-        if (inByte <= 127) // This prevents Linux ttyACM driver to fail
-        {
-          states = ((sensorHighByte & 0x07) << 7) | (inByte & 0x7F);
-     
-          if (states == 1) {
-            the_value = 1;
-            digitalWrite(pin, states);
-            ScratchBoardSensorReport(msg_act_pin,0);//scratch recved, so action set to 0
-       
-          }
-
-        }
-      }
-      else {
-        while (!Serial.available()); // Wait for the end of the word with data
-        inByte = Serial.read();
-        if (inByte <= 127) // This prevents Linux ttyACM driver to fail
-        {
-          digitalWrite(pin, states);
-          states = ((sensorHighByte & 0x07) << 7) | (inByte & 0x7F);
-        }
-      }
-    }
-  }
-  else checkScratchDisconnection();
+  if (millis() - lastDataReceivedTime > 1000) reset(); //reset state if actuators reception timeout = one second
 }
-
 void reset() //with xbee module, we need to simulate the setup execution that occurs when a usb connection is opened or closed without this module
 {
   configurePins();
-  ScratchBoardSensorReport(handshaking_pin, the_value);
+  ScratchBoardSensorReport(handshaking_pin, 0);
   lastDataReceivedTime = millis();
+}
+void device_control (int _ID[], char turn)//Sarah 001
+{
+  deviceID += String( _ID[0], HEX);
+  deviceID += turn;
+  wifitoserver.print(deviceID);
 }
